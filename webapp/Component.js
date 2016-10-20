75e48e145da9a3458c8999e17db4b27e176b0f1b
Gregor Wolf <wolf.gregor@web.de>
2016-10-20 17:18:50 +0000

    Adding the app to the repository

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("sap.ui.generic.template.sample.SmartTemplate.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("sap.ui.generic.template.sample.SmartTemplate.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
